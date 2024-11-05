ddf9e09ab0ef9bbf341602156091f6b92acad973
boyden zhou <boydenz1999@gmail.com>
2024-11-04 20:13:17 -0800

    adjutsed server.js to use firebase instead of mongoose, moved connection to db from post-controller to server.js

--- hauler-backend/middleware/auth.js
+++ hauler-backend/middleware/auth.js
@@ -1,14 +1,17 @@
-// middleware/auth.js
 const admin = require('firebase-admin');
 
 async function checkUserRole(role) {
   return async (req, res, next) => {
     const token = req.headers.authorization.split('Bearer ')[1];
-    const decodedToken = await admin.auth().verifyIdToken(token);
-    if (decodedToken.role === role) {
-      next();
-    } else {
-      res.status(403).send('Forbidden');
+    try {
+      const decodedToken = await admin.auth().verifyIdToken(token);
+      if (decodedToken.role === role) {
+        next();
+      } else {
+        res.status(403).send('Forbidden');
+      }
+    } catch (error) {
+      res.status(401).send('Unauthorized');
     }
   };
 }
--- hauler-backend/server.js
+++ hauler-backend/server.js
@@ -1,68 +1,36 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
-// const bodyParser = require("body-parser");
+const admin = require('firebase-admin');
 const stripe = require('stripe')('sk_test_51M23WVAZXbnAuaLLJktMTrL2oSSQKCpqvjDDHkAK1PaYlJMFtLevnKFM9qUTjl6PjS9O3F4jGv7LsX9Yp1XUcRbR00G8JLajvz');
+const firestore = admin.firestore();
 
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || "3000";
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'hauler-db.firebaseapp.com'
+});
 
-//importing routes
+// Importing routes
 const postRoutes = require('./routes/post-routes.js');
 const serviceProvidersRoutes = require('./routes/serviceProvider-router.js');
 const userRoutes = require('./routes/user-router.js');
-const stripeRoutes = require('./routes/stripe-routes.js')
+const stripeRoutes = require('./routes/stripe-routes.js');
 
-//importing middleware
-// app.use(bodyParser.json());
+// Middleware
 app.use(express.json());
 app.use(cors());
-// app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ extended: true }));
 
-//defining restApi's
+// Defining API routes
 app.use('/api/posts', postRoutes);
 app.use('/api/service-providers', serviceProvidersRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api/stripe', stripeRoutes)
+app.use('/api/stripe', stripeRoutes);
 
-
-//connecting to MongoAtlas
-const uri = process.env.MONGO_URI;
-pipeline = [
-    {
-        $match: {
-            "updateDescription.updatedFields": {
-                "status": "Complete"
-            }
-        }
-    }];
-const options = { fullDocument: "updateLookup" };
-const PostModel = require('./models/posts')
-const postChangeEventEmitter = PostModel.watch(pipeline, options)
-postChangeEventEmitter.on('change', (change) => {
-
-    paymentIntent = change.fullDocument.paymentIntent
-    console.log('change.fullDocument.paymentIntent', change.fullDocument.paymentIntent)
-
-    amountToCapture = change.fullDocument.acceptedPrice * 100;
-
-    stripe.paymentIntents.capture(paymentIntent, {
-        amount_to_capture: amountToCapture,
-    })
-        .then(console.log("successssssssss"))
-})
-
-mongoose
-    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        app.set("port", port);
-        app.listen(port, () =>
-            console.log(`App running successfully on port http://localhost:${port}`)
-        );
-    })
-    .catch(err => {
-        console.log(err)
-    });
+// Start the server
+app.listen(port, () => {
+  console.log(`App running successfully on port http://localhost:${port}`);
+});
