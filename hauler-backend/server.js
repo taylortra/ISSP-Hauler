28185efa5e48e947f964674b1c509d966d277172
boyden zhou <boydenz1999@gmail.com>
2024-11-18 10:17:45 -0800

    added svc key, moved const firestore to after connecting to firebase

--- hauler-backend/server.js
+++ hauler-backend/server.js
@@ -2,14 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const admin = require('firebase-admin');
 const stripe = require('stripe')('sk_test_51M23WVAZXbnAuaLLJktMTrL2oSSQKCpqvjDDHkAK1PaYlJMFtLevnKFM9qUTjl6PjS9O3F4jGv7LsX9Yp1XUcRbR00G8JLajvz');
-const firestore = admin.firestore();
 
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || "3000";
 
-// do we have a service account key initialized somewhere?? 
-// const serviceAccount = require(serviceAccountKey.json)
+var serviceAccount = require("./hauler-db-firebase-adminsdk-f30st-ab4446ec88.json");
 
 // Connect to Firebase
 admin.initializeApp({
@@ -17,18 +15,19 @@ admin.initializeApp({
   databaseURL: 'hauler-db.firebaseapp.com'
 });
 
+const firestore = admin.firestore();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
 // Importing routes
 const postRoutes = require('./routes/post-routes.js');
 const serviceProvidersRoutes = require('./routes/serviceProvider-router.js');
 const userRoutes = require('./routes/user-router.js');
 const stripeRoutes = require('./routes/stripe-routes.js');
 
-// Middleware
-app.use(express.json());
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-
-// Defining API routes
+// Using routes
 app.use('/api/posts', postRoutes);
 app.use('/api/service-providers', serviceProvidersRoutes);
 app.use('/api/users', userRoutes);
@@ -36,5 +35,5 @@ app.use('/api/stripe', stripeRoutes);
 
 // Start the server
 app.listen(port, () => {
-  console.log(`App running successfully on port http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
